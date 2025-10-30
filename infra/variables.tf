984f2c62896b2d9058962c2ba2a159bb700c886c
doxajay <99846671+doxajay@users.noreply.github.com>
2025-10-30 13:08:41 -0600

    Add key_name variable for EC2 instance
    Added key_name variable for EC2 SSH access.

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,10 @@ variable "allowed_cidr" {
   type        = string
   default     = "0.0.0.0/0"
 }
+# Key pair name for connecting to the EC2 instance
+variable "key_name" {
+  description = "The name of the existing EC2 key pair to use for SSH access"
+  type        = string
+  default     = "acme-key"  # <-- Replace this with your actual AWS key pair name
+}
+
