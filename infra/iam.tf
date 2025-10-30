3f55b18f00a99718eddbf2f4a2d10ecba8f37165
doxajay <99846671+doxajay@users.noreply.github.com>
2025-10-29 22:11:19 -0600

    Refactor IAM role and policy document structure

--- infra/iam.tf
+++ infra/iam.tf
@@ -6,12 +6,17 @@ resource "aws_iam_role" "eks_role" {
 data "aws_iam_policy_document" "eks_assume" {
   statement {
     effect = "Allow"
-    principals { type = "Service", identifiers = ["eks.amazonaws.com"] }
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
     actions = ["sts:AssumeRole"]
   }
 }
+
 
 resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
