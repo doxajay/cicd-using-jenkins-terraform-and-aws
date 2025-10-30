80eee2570eaa802582c6ba4cce7238170baa738c
doxajay <99846671+doxajay@users.noreply.github.com>
2025-10-29 22:15:25 -0600

    Update EKS cluster to include second public subnet
    Added support for an additional public subnet in the EKS cluster configuration.

--- infra/eks.tf
+++ infra/eks.tf
@@ -2,7 +2,10 @@ resource "aws_eks_cluster" "main" {
   name     = "acme-eks-jenkins-cluster"
   role_arn = aws_iam_role.eks_role.arn
   vpc_config {
-    subnet_ids = [aws_subnet.public_1.id]
+    subnet_ids = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
+    ]
   }
   depends_on = [aws_iam_role_policy_attachment.eks_policy]
 }
