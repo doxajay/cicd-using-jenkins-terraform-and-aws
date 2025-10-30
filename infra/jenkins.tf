67eba0e5693f7a34d8426cdd1dc6fc3bc18dcde5
doxajay <99846671+doxajay@users.noreply.github.com>
2025-10-29 22:08:05 -0600

    Add Jenkins server and security group configuration
    This configuration sets up an AWS Jenkins server with a security group allowing HTTP and SSH access.

---
+++ infra/jenkins.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 in us-west-2
+  instance_type = "t3.medium"
+  subnet_id     = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  key_name      = null
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y java-11-amazon-corretto
+              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+              rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+              yum install -y jenkins git docker
+              systemctl enable jenkins
+              systemctl start jenkins
+              EOF
+
+  tags = { Name = "jenkins-server" }
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name   = "jenkins-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_cidr]
+  }
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
