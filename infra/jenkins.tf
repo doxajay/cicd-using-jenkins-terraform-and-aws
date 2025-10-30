5bef23acd2d88d2384962426162e2da1b64f4446
doxajay <99846671+doxajay@users.noreply.github.com>
2025-10-29 22:33:51 -0600

    Modify Jenkins instance and security group settings
    Updated Jenkins instance configuration and security group settings.

--- infra/jenkins.tf
+++ infra/jenkins.tf
@@ -1,53 +1,23 @@
 resource "aws_instance" "jenkins" {
-  ami           = "ami-0892d3c7ee96c0bf7"  # Amazon Linux 2 in us-west-2
-  instance_type = "t3.medium"
+  ami           = "ami-0e1d30f2c40c4c701"  # Amazon Linux 2 (for us-west-2)
+  instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_1.id
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-  key_name      = null
+  associate_public_ip_address = true
+  key_name = "your-keypair-name"  # replace with your real EC2 key pair name
 
   user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y java-11-amazon-corretto
-              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-              rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-              yum install -y jenkins git docker
-              systemctl enable jenkins
-              systemctl start jenkins
-              EOF
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras install java-openjdk11 -y
+    wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    yum install jenkins -y
+    systemctl enable jenkins
+    systemctl start jenkins
+  EOF
 
-  tags = { Name = "jenkins-server" }
-}
-
-resource "aws_security_group" "jenkins_sg" {
-  name   = "jenkins-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
-  }
-
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.allowed_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "jenkins-server"
   }
 }
-
-output "jenkins_public_ip" {
-  value = aws_instance.jenkins.public_ip
-}
-
